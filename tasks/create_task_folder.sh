87a8d66f0d900664eec8c34c958c4c234a065e26
Daniel Firebanks-Quevedo <dfireban@oberlin.edu>
2020-11-28 00:51:22 -0500

    Added script to automatically create folders with input/output/src in them

--- tasks/create_task_folder.sh
+++ tasks/create_task_folder.sh
@@ -1,3 +1,33 @@
+#!/bin/bash
 
+
+FOLDERS_TO_CREATE="input output src"
+
+for i in "$@"
+do
+case $i in
+    -n=*|--name=*)
+    FOLDER_NAME="${i#*=}"
+    mkdir ${FOLDER_NAME}
+    cd ${FOLDER_NAME}
+    for folder in $FOLDERS_TO_CREATE; do
+        mkdir $folder
+        cd $folder
+        touch .gitkeep
+	cd ..
+    done
+    shift # past argument=value
+    ;;
+    -a=*|--add_notebook=*)
+    ADD_NOTEBOOK="${i#*=}"
+    if [[ $ADD_NOTEBOOK == "True" ]]; then
+	mkdir notebooks
+	cd notebooks
+	touch .gitkeep
+	cd .. 
+    fi	
+    ;;
+esac
+done
 
 
