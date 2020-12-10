b784433332377fa661b89dd6d0928e83fbc31f75
Daniel Firebanks-Quevedo <dfireban@oberlin.edu>
2020-12-09 23:57:10 -0500

    Sync order of model predictions and dataset

--- tasks/data_loader/src/utils.py
+++ tasks/data_loader/src/utils.py
@@ -7,35 +7,38 @@ def load_file(file_name):
 
 
 def labeled_sentences_from_dataset(dataset):
-    sentence_tags_pairs = []
-                    
+    sentence_tags_dict = {}
+
     for document in dataset.values():
         for section in document.values():
-            for subsection in section.values():
-                for sentence in subsection['sentences'].values():
-                    sentence_tags_pairs.append((sentence['text'], sentence['labels'][0]))
-                    
-    return sentence_tags_pairs
+            sentence_tags_dict.update(section['sentences'])
+
+    return sentence_tags_dict
 
 
-def labeled_sentences_from_model_output(model_preds):
-    sentence_tags_pairs = []
-    for preds in model_preds:
-        sentence_tags_pairs.append((preds["text"], preds["labels"][0]))
-    return sentence_tags_pairs
+def sort_model_preds(dataset, model_preds):
+    """
+    Sorts the model predictions in the order that the input dataset is in.
+    """
+    # Dictionaries are insertion ordered since Python 3.6+,
+    ordered_preds = {}
+
+    for sentence_id in dataset:
+        ordered_preds[sentence_id] = model_preds[sentence_id]
+
+    return ordered_preds
 
 
 def labels_from_dataset(dataset):
     labels = []
-                    
+
     for document in dataset.values():
         for section in document.values():
-            for subsection in section.values():
-                for sentence in subsection['sentences'].values():
-                    labels.append(sentence['labels'][0])
-                    
+            for sentence in section['sentences'].values():
+                labels.append(sentence['labels'][0])
+
     return labels
 
 
 def labels_from_model_output(model_preds):
-    return [preds["labels"][0] for preds in model_preds]
+    return [preds["labels"][0] for preds in model_preds.values()]
