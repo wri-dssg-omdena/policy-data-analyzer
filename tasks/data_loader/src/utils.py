87420cc1b58b30c845a881bd22fa586beb2df5b9
Daniel Firebanks-Quevedo <dfireban@oberlin.edu>
2020-12-21 02:06:42 -0500

    Finished topic classification experiments for zero shot, added data loader utils

--- tasks/data_loader/src/utils.py
+++ tasks/data_loader/src/utils.py
@@ -1,4 +1,6 @@
 import json
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def load_file(file_name):
@@ -14,6 +16,24 @@ def labeled_sentences_from_dataset(dataset):
             sentence_tags_dict.update(section['sentences'])
 
     return sentence_tags_dict
+
+
+def numeric_labels_from_dataset(dataset):
+    """
+    TEMPORARY: We're getting the set of unique labels from the data, but in reality we should standardize this and define it in a separate env file.
+    """
+    dataset_labels = labels_from_dataset(dataset)
+    label_names = [label for label in set(dataset_labels)]
+    label_names.sort()
+    label_map = dict(zip(label_names, range(len(label_names))))
+    num_dataset_labels = [label_map[label] for label in dataset_labels]
+    return num_dataset_labels
+
+
+def labels_to_numeric(labels, label_names):
+    label_map = dict(zip(label_names, range(len(label_names))))
+    num_dataset_labels = [label_map[label] for label in labels]
+    return num_dataset_labels
 
 
 def sort_model_preds(dataset, model_preds):
@@ -42,3 +62,33 @@ def labels_from_dataset(dataset):
 
 def labels_from_model_output(model_preds):
     return [preds["labels"][0] for preds in model_preds.values()]
+
+
+def get_counts_per_label(y_true, n_classes):
+    """
+    Return a map of {label: number of data points with that label} for the given list of labels
+
+    Parameters:
+        - y_true: (integer) a list of labels
+        - n_classes: (integer) the number of classes
+    """
+    label_counts = [0] * n_classes
+    for label in y_true:
+        label_counts[label] += 1
+    return label_counts
+
+
+def plot_data_distribution(data, label_names, normalize=True):
+    weights = np.array(get_counts_per_label(data, len(label_names)))
+    if normalize:
+        weights = weights / sum(weights)
+
+    plt.bar(label_names, weights)
+    plt.xticks(label_names, rotation=90)
+    plt.title("Data Distribution")
+    plt.xlabel("Label")
+    plt.ylabel("Percentage of label in data")
+    plt.show()
+
+    print("Label counts:")
+    print(dict(zip(label_names, weights)))
