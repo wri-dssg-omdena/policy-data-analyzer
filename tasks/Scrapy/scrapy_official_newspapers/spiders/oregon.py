4701739c7421be5aa393d7c87a7108f3a37536ec
Jordi Planas <jordi.planasc@gmail.com>
2021-02-18 16:13:04 +0100

    refacoring of Oregon spider

--- tasks/Scrapy/scrapy_official_newspapers/spiders/oregon.py
+++ tasks/Scrapy/scrapy_official_newspapers/spiders/oregon.py
@@ -19,6 +19,7 @@ from collections import defaultdict
 from scrapy_official_newspapers.spiders import BaseSpider
 from scrapy_official_newspapers.items import ScrapyOfficialNewspapersItem
 import scrapy
+from dateutil.parser import parse
 
 
 class OregonSpider(BaseSpider):
@@ -26,60 +27,65 @@ class OregonSpider(BaseSpider):
     allowed_domains = ['secure.sos.state.or.us']
     start_urls = ['https://secure.sos.state.or.us/oard/displayBulletins.action']
     country = "USA"
-    #state = "Oregon"
+    state = "Oregon"
     data_source = "The Oregon Bulletin"
+    scrapable = "True"
     # years = [year for year in range(2018, 2020)]
     spider_builder = "David Silva"
 
-    def __init__(self, date = "2021-02-11"):
+    def __init__(self, date = "2020-12-11"):
         self.keyword_dict = self.import_json('./keywords_and_dictionaries/keywords_knowledge_domain.json')
         self.negative_keyword_dict = self.import_json('./keywords_and_dictionaries/negative_keywords_knowledge_domain.json')
         self.from_date, self.today = self.create_date_span(date)
 
+
     def parse(self, response):        
         # Iterate over URLs
         i = 0
-        for href in response.css('div#accordion').css('a::text'):#attr(href)
-            i += 1
-            url = response.urljoin(href.extract())
-            self.debug(url)
-            #yield scrapy.Request(url, callback=self.parse_month_bulletin)
+        for bulletin in response.css('div#accordion').css('a'):
+            # Get the url and the text of the element, the textbeing the publication date
+            self.url = response.urljoin(bulletin.css('::attr(href)').get())
+            publication_date = bulletin.css('::text').get().replace("\xa0\xa0", "-")
+            # Filter by date and request
+            if parse(self.from_date) <= parse(publication_date) <= parse(self.today):
+                #self.debug(publication_date)
+                yield scrapy.Request(self.url, callback=self.parse_month_bulletin)
     
-    #def parse_month_bulletin(self, response):
-    #    # Get all table headers of the web page
-    #    total_headers = response.css('table').css('thead').css('th::text').extract()
-    #    # item = ScrapyOfficialNewspapersItem()
-    #    item = defaultdict.fromkeys(total_headers)
+    def parse_month_bulletin(self, response):
+        # Get all table headers of the web page
+        total_headers = response.css('table').css('thead').css('th::text').extract_first()
+        item = ScrapyOfficialNewspapersItem()
+        #item = defaultdict.fromkeys(total_headers)
 
-    #    # Iterate over tables
-    #    for table in response.css('table'):
-    #        id = table.attrib['id']  # Notices or filings
-    #        table_headers = table.css('thead').css('th::text').extract()
-    #        # Iterate over table rows
-    #        for tr in table.css('tbody').css('tr'):
-    #            # Get all text in a single row of the table
-    #            row_values = tr.css('td::text').getall()
-    #            row_values = list(filter(None, map(lambda x: " ".join(x.split()), row_values)))
-    #            assert len(table_headers) == len(row_values), 'More row values than table headers!'
-    #            # Get the link of a single row of the table
-    #            doc_url = response.urljoin(tr.css('a::attr(href)').extract_first())
-    #            # Populate item dictionary
-    #            item['id'] = id
-    #            item.update(dict(zip(table_headers, row_values)))
-    #            item['country'] = self.country
-    #            # item['geo_code'] = self.geo_code
-    #            # item['level'] = self.level
-    #            item['data_source'] = self.data_source
-    #            # item['title'] = resume
-    #            # item['reference'] = reference
-    #            # item['authorship'] = str(authorship)
-    #            # item['resume'] = resume
-    #            # item['publication_date'] = date
-    #            # item['enforcement_date'] = date
-    #            # item['url'] = response.url
-    #            item['doc_url'] = doc_url
-    #            # item['doc_name'] = reference+'html'
-    #            # item['doc_type'] = self.doc_type
-    #            # item['doc_class'] = ''
-    #            # item['file_urls'] = [doc_url]
-    #            yield item
+        # We take only the first table and we could loop through the two tables if there is a need to get the information of the second table
+        table = response.css('table')[0]
+        id = table.attrib['id']  # Notices or filings
+        table_headers = table.css('thead').css('th::text').extract()
+        self.debug(table_headers)
+        ## Iterate over table rows
+        for tr in table.css('tbody').css('tr'):
+            # Get all text in a single row of the table
+            row_values = tr.css('td::text').getall()
+            row_values = list(filter(None, map(lambda x: " ".join(x.split()), row_values)))
+            assert len(table_headers) == len(row_values), 'More row values than table headers!'
+            # Get the link of a single row of the table
+            doc_url = response.urljoin(tr.css('a::attr(href)').extract_first())
+            # Populate item dictionary
+            #item.update(dict(zip(table_headers, row_values)))
+            self.debug(row_values)
+            text_to_search = row_values[3]
+            if self.search_keywords(text_to_search, self.keyword_dict, self.negative_keyword_dict) or self.scrapable == "True":
+                item['country'] = self.country
+                item['state'] = self.state
+                item['data_source'] = self.data_source
+                item['law_class'] = ''
+                item['title'] = row_values[3]
+                item['reference'] = ''
+                item['authorship'] = row_values[1]
+                item['summary'] = ''
+                item['publication_date'] = row_values[2].split(' ')[0]
+                item['url'] = response.url
+                item['doc_url'] = doc_url
+                item['doc_name'] = self.HSA1_encoding(doc_url)
+                yield item
+                                                   
