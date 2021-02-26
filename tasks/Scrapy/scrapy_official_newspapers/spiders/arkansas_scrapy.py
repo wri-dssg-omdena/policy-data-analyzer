427c76edabd6e8b1df7270dccc7cae8774c3080a
Rong Fang <34821426+rongfang323@users.noreply.github.com>
2021-02-26 08:53:09 -0800

    Update arkansas_scrapy.py

--- tasks/Scrapy/scrapy_official_newspapers/spiders/arkansas_scrapy.py
+++ tasks/Scrapy/scrapy_official_newspapers/spiders/arkansas_scrapy.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from arkansas_policy_scrapy.items import ArkanasaPolicyScrapyItem
+from arkansas_policy_scrapy.items import ArkansasPolicyScrapyItem
 from scrapy.selector import Selector
 from scrapy.http import HtmlResponse
 from scrapy.http import Request
