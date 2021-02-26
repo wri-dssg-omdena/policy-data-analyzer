d5f84911ef5d7bb2a1cd8d3779267bc1d2f289ef
Jordi Planas <jordi.planasc@gmail.com>
2021-02-26 07:26:53 +0100

    India code almost ready

--- tasks/Scrapy/scrapy_official_newspapers/spiders/IndiaCodeActs.py
+++ tasks/Scrapy/scrapy_official_newspapers/spiders/IndiaCodeActs.py
@@ -8,7 +8,7 @@ class IndiaCodeActs(BaseSpider):
     name = "IndiaCodeActs"
     country = "India"
     country_code = "IN" # You can find the ISO3166 country code here: https://gist.github.com/ssskip/5a94bfcd2835bf1dea52
-    state_name = "Federal&State"
+    state_name = "Federal"
     satate_code = "" # As per the Holidays package, you can find the code here https://pypi.org/project/holidays/ if avaiable.
     source = "India Code"
     spider_builder = "Jordi Planas"
@@ -23,12 +23,13 @@ class IndiaCodeActs(BaseSpider):
         self.negative_keyword_dict = self.import_json('./keywords_and_dictionaries/negative_keywords_knowledge_domain_ES.json')
         # This is to set the time span variables. 
         self.from_date, self.today = self.create_date_span(self.start_date)
-    
+
     def start_requests(self):
         from_year = datetime.datetime.strptime(self.from_date, '%Y-%m-%d').year
         to_year = datetime.datetime.strptime(self.today, '%Y-%m-%d').year
         start = 0
-        start_url = f'https://www.indiacode.nic.in/simple-search?query=forestry&sort_by=score&order=desc&rpp=1000&etal=0&filtername=actyear&filterquery=%5B{from_year}+TO+{to_year}%5D&filtertype=equals'
+        start_url = f'https://www.indiacode.nic.in/handle/123456789/1362/simple-search?page-token=c2035d749fe0&page-token-value=e8416433108e80650fe97479b9d11464&nccharset=5CA7ECED&location=%2F&query=forestry&rpp=10&sort_by=score&order=desc&filter_field_1=actyear&filter_type_1=equals&filter_value_1=%5B2000+TO+2020%5D'
+        # f'https://www.indiacode.nic.in/simple-search?query=forestry&sort_by=score&order=desc&rpp=1000&etal=0&filtername=actyear&filterquery=%5B{from_year}+TO+{to_year}%5D&filtertype=equals'
         yield scrapy.Request(start_url, dont_filter=True, callback=self.parse)
         #for i in range(0, len(self.keyword_dict)):
         #    self.debug(i)
@@ -55,61 +56,49 @@ class IndiaCodeActs(BaseSpider):
                 pass
             else:
                 self.done_dictionary[self.title] = 0
-                i += 1
-                #print('*** ', i, ' ***')
-                self.publication_date = tr.css('td::text')[0].get()
-                self.title = tr.css('td::text')[1].get()
-                details_url = response.urljoin(tr.css('td').css('a::attr(href)').get())
-                yield scrapy.Request(details_url, dont_filter=True, callback=self.parse_other)
-                # print("\n", self.publication_date, " ** ", self.title, "\n")
-                # print(details_url)
-# //*[@id="content"]/div/div/div[1]/div/div[4]/div/table/tbody/tr[2]/td[1]
-    #     for granule in json.loads(response.text)["granules"]:
-    #         url = granule["granuleLink"] + f'?api_key={self.API_key}'
-    #         yield scrapy.Request(url, dont_filter=True, callback=self.parse_other)
+                self.details_url = response.urljoin(tr.css('td').css('a::attr(href)').get())
+                yield scrapy.Request(self.details_url, dont_filter=True, callback=self.parse_other)
 
     def parse_other(self, response):
-        doc_url =  response.urljoin(response.css('#short_title').css('a::attr(href)').get())
-        doc_name = self.HSA1_encoding(doc_url) + doc_url.split('#')[0][-4:]
-        self.debug(doc_name)
+        flag = True
         for tr in response.css('#tb2 table tr'):
-            if "Act ID" in tr.css('td')[0].get():
-                reference = tr.css('td')[1].get()
-            if "Ministry" in tr.css('td')[0].get():
-                ministry = tr.css('td')[1].get()
-            if "Department" in tr.css('td')[0].get():
-                department = tr.css('td')[1].get()
             if "Location" in tr.css('td')[0].get():
-                state_name = tr.css('td')[1].get()
-            if "Long Title" in tr.css('td')[0].get():
+                self.state_name = tr.css('td::text')[1].get()
+            if "Act ID" in tr.css('td')[0].get():
+                reference = tr.css('td::text')[1].get()
+            if "Enactment" in tr.css('td::text')[0].get():
+                publication_date = tr.css('td')[1].get()
+            if "Short Title" in tr.css('td::text')[0].get():
+                title = tr.css('td')[1].get()
+            if "Long Title" in tr.css('td::text')[0].get():
                 summary = tr.css('td')[1].get()
-            else:
-                summary = ""
+                Flag = False
+            if "Ministry" in tr.css('td')[0].get():
+                ministry = tr.css('td::text')[1].get()
+            if "Department" in tr.css('td')[0].get():
+                department = tr.css('td::text')[1].get()
+        if flag:
+            summary = ''
 
-            elif 
+        # text_to_search = self.title + " " + summary
+        # if self.search_keywords(text_to_search, self.keyword_dict, self.negative_keyword_dict):
+        item = ScrapyOfficialNewspapersItem()
 
+        item['country'] = self.country
+        item['state'] = self.state_name
+        item['data_source'] = self.source
+        item['law_class'] = "Act"
+        item['title'] = title
+        item['reference'] = reference
+        item['authorship'] = ministry + "/" + department
+        item['summary'] = summary
+        item['publication_date'] = publication_date
+        item['url'] = self.details_url
+        doc_url = response.urljoin(response.css('#short_title').css('a::attr(href)').get())
+        item['doc_url'] = doc_url
+        item['doc_name'] = self.HSA1_encoding(doc_url) + doc_url.split('#')[0][-4:]
 
-        #print(response.css("div#tb2 table tr").get())
-        summary = response.xpath('//*[@id="tb2"]/div/div/table/tr[6]/td[2]/text()').get()
-        self.debug(summary)
-        text_to_search = self.title + " " + summary
-        if self.search_keywords(text_to_search, self.keyword_dict, self.negative_keyword_dict):
-            item = ScrapyOfficialNewspapersItem()
-
-            item['country'] = self.country
-            item['state'] = self.state_name
-            item['data_source'] = self.source
-            item['law_class'] = summary_full['category']
-            item['title'] = summary_full['title']
-            item['reference'] = summary_full['granuleId']
-            item['authorship'] = summary_full['agencies'][0]['name']
-            item['summary'] = summary
-            item['publication_date'] = summary_full['dateIssued']
-            item['url'] = summary_full['download']['txtLink'].replace('htm', 'summary?api_key=')
-            item['doc_url'] = summary_full['download']['txtLink'] + '?api_key='
-            item['doc_name'] = self.HSA1_encoding(summary_full['download']['txtLink'] + f'?api_key={self.API_key}')
-
-    #         yield item
+        yield item
 
 
 
