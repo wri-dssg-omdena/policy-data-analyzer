94b2eb885570660d0f3608447ab37b6127d17271
Daniel Firebanks-Quevedo <dfireban@oberlin.edu>
2021-03-15 00:50:56 -0400

    Updated labeled sentences data loading function

--- tasks/data_loading/src/utils.py
+++ tasks/data_loading/src/utils.py
@@ -12,8 +12,7 @@ def labeled_sentences_from_dataset(dataset):
     sentence_tags_dict = {}
 
     for document in dataset.values():
-        for section in document.values():
-            sentence_tags_dict.update(section['sentences'])
+        sentence_tags_dict.update(document['sentences'])
 
     return sentence_tags_dict
 
