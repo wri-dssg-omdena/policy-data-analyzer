51fa2181f9a08e5b6edd2467b0eb66168cb1d47e
Ramansh Sharma <sharmar@bxscience.edu>
2021-04-25 00:10:03 +0530

    adding wandb sweeps for hyperparam tuning

--- tasks/fine_tuning_sbert/src/loops.py
+++ tasks/fine_tuning_sbert/src/loops.py
@@ -69,34 +69,26 @@ class SoftmaxClassifier(nn.Module):
             return features, output
 
 
-def grid_search_fine_tune_sbert(train_params, train_sents, train_labels, label_names,
-                                eval_classifier=None):
+def grid_search_fine_tune_sbert(config):
     """
     Find the optimal SBERT model by doing a hyperparameter search over random seeds, dev percentage, and different types of SBERT models
     """
-    output_path = train_params["output_path"]
-    all_dev_perc = train_params["all_dev_perc"]
-    model_names = train_params["model_names"]
-    max_num_epochs = train_params["max_num_epochs"]
-    baseline = train_params['baseline']
-    patience = train_params['patience']
-    seeds = train_params['seeds']
-    wandb_key = train_params['wandb_key']
-
-    os.environ['WANDB_API_KEY'] = wandb_key
-
-    if eval_classifier is None:
-        train_params["eval_classifier"] = "SBERT"
-    else:
-        train_params["eval_classifier"] = eval_classifier.__class__.__name__
+    train_sents = config.train_sents
+    train_labels = config.train_labels
+    label_names = config.label_names
 
     print(
-        f"Grid Search Fine tuning parameters:\n{json.dumps(train_params, indent=4)}")
+        f"Grid Search Fine tuning parameters:\n{json.dumps(config, indent=4)}")
 
     label2int = dict(zip(label_names, range(len(label_names))))
 
-    for dev_perc in all_dev_perc:
-        X_train, X_dev, y_train, y_dev = train_test_split(train_sents, train_labels, test_size=dev_perc,
+    model_deets = f"{config.eval_classifier}_model={config.model_name}_test-perc={config.dev_perc}_seed={config.seeds}"
+    # this will write to the same project every time
+
+    with wandb.init(config=config, notes=model_deets, project='WRI', tags=['baseline', 'training'],
+                    entity='ramanshsharma'):
+
+        X_train, X_dev, y_train, y_dev = train_test_split(train_sents, train_labels, test_size=config.dev_perc,
                                                           stratify=train_labels, random_state=100)
 
         # Load data samples into batches
@@ -104,60 +96,53 @@ def grid_search_fine_tune_sbert(train_params, train_sents, train_labels, label_n
         train_samples = build_data_samples(X_train, label2int, y_train)
         dev_samples = build_data_samples(X_dev, label2int, y_dev)
 
-        for model_name in model_names:
-            # Train set config
-            model = EarlyStoppingSentenceTransformer(model_name)
-            train_dataset = SentencesDataset(train_samples, model=model)
-            train_dataloader = DataLoader(
-                train_dataset, shuffle=True, batch_size=train_batch_size)
+        # Train set config
+        model = EarlyStoppingSentenceTransformer(config.model_name)
+        train_dataset = SentencesDataset(train_samples, model=model)
+        train_dataloader = DataLoader(
+            train_dataset, shuffle=True, batch_size=train_batch_size)
 
-            # Dev set config
-            dev_dataset = SentencesDataset(dev_samples, model=model)
-            dev_dataloader = DataLoader(
-                dev_dataset, shuffle=True, batch_size=train_batch_size)
+        # Dev set config
+        dev_dataset = SentencesDataset(dev_samples, model=model)
+        dev_dataloader = DataLoader(
+            dev_dataset, shuffle=True, batch_size=train_batch_size)
 
-            # Define the way the loss is computed
-            classifier = SoftmaxClassifier(model=model,
-                                           sentence_embedding_dimension=model.get_sentence_embedding_dimension(),
-                                           num_labels=len(label2int))
-            warmup_steps = math.ceil(
-                len(train_dataset) * max_num_epochs / train_batch_size * 0.1)  # 10% of train data for warm-up
+        # Define the way the loss is computed
+        classifier = SoftmaxClassifier(model=model,
+                                       sentence_embedding_dimension=model.get_sentence_embedding_dimension(),
+                                       num_labels=len(label2int))
+        warmup_steps = math.ceil(
+            len(train_dataset) * config.max_num_epochs / train_batch_size * 0.1)  # 10% of train data for warm-up
 
-            for seed in seeds:
-                set_seeds(seed)
-                model_deets = f"{train_params['eval_classifier']}_model={model_name}_test-perc={dev_perc}_seed={seed}"
+        set_seeds(config.seeds)
 
-                # Train the model
-                start = time.time()
-                dev_evaluator = CustomLabelAccuracyEvaluator(dataloader=dev_dataloader, softmax_model=classifier,
-                                                             name='lae-dev', label_names=label_names,
-                                                             model_hyper_params={'model_name': model_name, 'dev_perc': dev_perc, 'seed': seed})
+        # Train the model
+        start = time.time()
+        dev_evaluator = CustomLabelAccuracyEvaluator(dataloader=dev_dataloader, softmax_model=classifier,
+                                                     name='lae-dev', label_names=label_names,
+                                                     model_hyper_params={'model_name': config.model_name, 'dev_perc': config.dev_perc, 'seed': config.seeds})
 
-                # this will write to the same project every time
-                wandb.init(notes=model_deets, project='WRI', tags=['baseline', 'training'],
-                           entity='ramanshsharma')
+        wandb.watch(model, log='all')
 
-                wandb.watch(model, log='all')
+        model.fit(train_objectives=[(train_dataloader, classifier)],
+                  evaluator=dev_evaluator,
+                  epochs=config.max_num_epochs,
+                  evaluation_steps=1000,
+                  warmup_steps=warmup_steps,
+                  output_path=config.output_path,
+                  model_deets=model_deets,
+                  baseline=config.baseline,
+                  patience=config.patience,
+                  )
 
-                model.fit(train_objectives=[(train_dataloader, classifier)],
-                          evaluator=dev_evaluator,
-                          epochs=max_num_epochs,
-                          evaluation_steps=1000,
-                          warmup_steps=warmup_steps,
-                          output_path=output_path,
-                          model_deets=model_deets,
-                          baseline=baseline,
-                          patience=patience,
-                          )
+        wandb.save(config.output_path)
+        wandb.finish()
 
-                wandb.save(output_path)
-                wandb.finish()
-
-                end = time.time()
-                hours, rem = divmod(end - start, 3600)
-                minutes, seconds = divmod(rem, 60)
-                print("Time taken for fine-tuning:",
-                      "{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
+        end = time.time()
+        hours, rem = divmod(end - start, 3600)
+        minutes, seconds = divmod(rem, 60)
+        print("Time taken for fine-tuning:",
+              "{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
 
 
 def build_data_samples(X_train, label2int, y_train):
@@ -189,7 +174,8 @@ def evaluate_using_sbert(model, test_sents, test_labels, label_names,
     Evaluate an S-BERT model on a previously unseen test set, visualizing the embeddings, confusion matrix,
     and returning. Evaluation method:
      - Calculate cosine similarity between label and sentence embeddings
-     - Includes the projection matrix approach used in https://joeddav.github.io/blog/2020/05/29/ZSL.html#A-latent-embedding-approach
+     #A-latent-embedding-approach
+     - Includes the projection matrix approach used in https://joeddav.github.io/blog/2020/05/29/ZSL.html
 
     """
     # Projection matrix Z low-dim projection
