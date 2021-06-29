b64e6f2e06bfea3010dabf93a48b60a6a6c7407b
Jordi Planas <jordi.planasc@gmail.com>
2021-06-29 11:49:00 +0200

    Update utils.py

--- tasks/data_loading/src/utils.py
+++ tasks/data_loading/src/utils.py
@@ -122,9 +122,9 @@ def load_training_dataset_HSSC(data_path, language, classification, labeling, da
                     raise Exception("Couldn't read file:", f)
             dataset.append(data[0].tolist())  # The data is always the entire first column
     if data_type == "train":
-        return dataset[2], dataset[3]
-    elif data_type == "test":
         return dataset[0], dataset[1]
+    elif data_type == "test":
+        return dataset[2], dataset[3]
 
 def load_json(file_name):
     with open(file_name, "r") as f:
